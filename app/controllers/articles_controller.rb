f73b55cae6915d324ce72067777059b851c73a7f
Penayo <penayo.gustavo@gmail.com>
2024-10-03 16:29:54 -0400

    feat(): Unimportant changes

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -13,9 +13,9 @@ class ArticlesController < ApplicationController
 
   def create
     @article = Article.new(article_params)
-    
+
     if @article.save
-      redirect_to @article      
+      redirect_to @article
     else
       render :new, status: :unprocessable_entity
     end
