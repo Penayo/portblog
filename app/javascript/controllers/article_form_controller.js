81eab67fd5d5663bad69af647e6310e70bfc1982
Penayo <penayo.gustavo@gmail.com>
2024-10-07 15:03:09 -0300

    fix(): Fixed code-block saving and highlighting
    I was setting up quill with incorrect syntax options. The "syntax: true"
    must be placed inside the module property and not inside the main
    option.

--- app/javascript/controllers/article_form_controller.js
+++ app/javascript/controllers/article_form_controller.js
@@ -17,12 +17,10 @@ const toolbarOptions = [
 ];
 
 const quilOptions = {
-  debug: "info",
-  syntax: true,
   modules: {
+    syntax: true,
     toolbar: toolbarOptions,
   },
-  placeholder: "Compose an epic...",
   theme: "snow",
 };
 
@@ -31,44 +29,18 @@ export default class extends Controller {
   quill;
 
   connect() {
-    const rootElement = this.element;
-
     this.quill = new Quill("#editor", quilOptions);
-    this.quill.on("editor-change", (eventName, ...args) => {
-      console.log("From Penayo", { eventName, args });
-    });
-
-    this.quill.on("text-change", function (delta, oldDelta, source, ...args) {
-      if (source === "user") {
-        // Check if the change is made by user input
-        // Check if the change includes a new code block
-        delta.ops.forEach((op) => {
-          if (op.attributes && op.attributes["code-block"]) {
-            console.log("A code block has been added!", op.attributes);
-            op.attributes["code-block"] = "javascript";
-            rootElement
-              .querySelectorAll("div.ql-code-block")
-              .forEach((block) => {
-                console.log("CODE BLOCK", block, window.hljs.listLanguages());
-                hljs.highlightAuto(block, { language: "ruby" });
-              });
-
-            // Handle anything you need to do when a code-block is detected
-          }
-        });
-      }
-    });
   }
 
   submit_form(event) {
     event.preventDefault();
-    const html = this.quill.root.innerHTML;
+
+    const html = this.quill.getSemanticHTML();
     const formElement = document.getElementById("article-form");
 
     const bodyElement = document.getElementsByName("article[body]")[0];
-    console.log(bodyElement);
-
-    bodyElement.value = html;
+    bodyElement.value = html
+    console.log({ value: bodyElement.value })
 
     formElement.submit();
   }
